6e16e387e07da0596e8bdd60210074525b53a8e5
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-02-03 15:41:58 -0800

    creating userModel

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const UserModel = require('../models/Users');
 
 // creating app
 const app = express();
