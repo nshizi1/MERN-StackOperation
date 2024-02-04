5f0d22278fa7fa75b97b73b70d93502e752d0346
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-02-03 16:14:05 -0800

    new records API

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,21 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 function CreateUser() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+  const navigate = useNavigate();
+
+  const Submit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/createUser", {name, email, age})
+    .then(result => {
+      console.log(result)
+      navigate("/");
+    })
+    .catch(err => console.log(err))
+  }
   return (
     <div className="flex h-screen bg-blue-500 justify-center items-center ">
       <div className="w-1/2 bg-white rounded-md p-3">
-        <form>
+        <form onSubmit={Submit}>
           <h2>Add User</h2>
           <div className="mb-2 flex gap-3">
             <label htmlFor="">Name</label>
-            <input type="text" className="p-2 border-2 rounded-sm " placeholder="Enter your names" />
+            <input type="text" className="p-2 border-2 rounded-sm " onChange={(e) => setName(e.target.value)} placeholder="Enter your names" />
           </div>
           <div className="mb-2 flex gap-3">
             <label htmlFor="">Email</label>
-            <input type="email" className="p-2 border-2 rounded-sm " placeholder="Enter your email" />
+            <input type="email" className="p-2 border-2 rounded-sm " onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
           </div>
           <div className="mb-2 flex gap-3">
             <label htmlFor="">Age</label>
-            <input type="text" className="p-2 border-2 rounded-sm " placeholder="Enter your age" />
+            <input type="text" className="p-2 border-2 rounded-sm " onChange={(e) => setAge(e.target.value)} placeholder="Enter your age" />
           </div>
           <button className="px-4 py-2 text-white bg-green-500 rounded-md">Add User</button>
         </form>
--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
 // requiring some packages
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const UserModel = require('../models/Users');
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./models/Users')
 
 // creating app
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 app.use(express.json())
 
 // connecting to MongoDB database
-mongoose.connect('mongodb://localhost:27017/crud');
+mongoose.connect('mongodb://localhost:27017/crud')
+
+
+app.post("/createUser", (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
 
 // running the server
 app.listen(3001, () => {
-    console.log(`Server is running!`);
-});
+    console.log(`Server is running!`)
+})
